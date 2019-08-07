d15d156f8f94aa4869aaef14dace038e228f72ab
Bill Forster <billforsternz@gmail.com>
2019-08-07 15:15:27 +1200

    Continuing the process or resyncing out of date thc-chess-library to Tarrasch updates, bring in Tarrasch Chess GUIs DebugPrintf.h and Portability.h (slight changes needed to avoid wwWidgets dependency)

--- src/Portability.h
+++ src/Portability.h
@@ -6,43 +6,26 @@
  ****************************************************************************/
 #ifndef PORTABILITY_H
 #define PORTABILITY_H
-
-//CHOOSE ONE:
-//#define TARRASCH_UNIX 1
-//#define TARRASCH_WINDOWS 1
-//#define TARRASCH_OSX 1
-
-#ifdef WIN32
-   #define TARRASCH_WINDOWS 1
+#include <stdint.h>     // int32_t etc. 
+#if defined _WIN32
+    #define THC_WINDOWS     // THC = triplehappy chess
 #else
-   #define TARRASCH_UNIX 1
+    #define THC_UNIX        // pretty good guess hopefully
 #endif
 
-#include <stdint.h>     // int32_t etc.
+#ifdef THC_WINDOWS
+   #define WINDOWS_FIX_LATER   // Windows only, fix later on Mac, Linux
+#endif
 
-#if TARRASCH_UNIX
-   #include <string.h>
-   typedef uint8_t byte;
-   #define THC_MAC
-   #define MAC_FIX_LATER
-   unsigned long GetTickCount();
-   int strcmpi(const char *s, const char *t);
-#elif TARRASCH_WINDOWS
+#ifdef THC_WINDOWS
    #include <Windows.h>
    #include <string.h>
    #define strcmpi _strcmpi
-   #define THC_WINDOWS         // Triple Happy Chess, Windows specific code
-   #define WINDOWS_FIX_LATER   // Windows only fix later on Mac
-#elif TARRASCH_OSX
-   unsigned long GetTickCount();
-   typedef unsigned char byte;
-   int strcmpi( const char *s, const char *t ); // return 0 if case insensitive match
-   #define THC_MAC             // Triple Happy Chess, Mac specific code
-   #define MAC_FIX_LATER       // Things not yet done on the Mac
 #else
-   #error Unknown Platform!
+   #include <string.h>
+   typedef unsigned char byte;
+   unsigned long GetTickCount();
+   int strcmpi( const char *s, const char *t ); // return 0 if case insensitive match
 #endif
-
-//#define DebugPrintfInner printf //temp
 
 #endif // PORTABILITY_H
